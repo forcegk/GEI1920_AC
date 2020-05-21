cfee4737b94e5d9fcaac3e31c56dc1a85e5725ab
Alonso <alonso.rodriguez@udc.es>
2020-05-21 17:55:08 +0200

    p3 con scripts

--- p3/informe/md_html.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-# generate-md is package markdown-styles from AUR or from
-# https://github.com/mixu/markdown-styles
-generate-md --layout github --input *.md --output .
---
+++ p3/informe/scripts/md_html.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+# YA NO USO ESTE
+# # generate-md is package markdown-styles from AUR or from
+# # https://github.com/mixu/markdown-styles
+# generate-md --layout github --input *.md --output .
+
+# ESTA ES LA NUEVA HERRAMIENTA QUE USO
+# https://github.com/KrauseFx/markdown-to-html-github-style
+cp ../*.md ./markdown-to-html-github-style/README.md
+cd markdown-to-html-github-style
+npm install
+cp README.html ../../informe_p3.html
+
